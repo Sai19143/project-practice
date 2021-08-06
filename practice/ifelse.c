ef4061d4302fd354337766ea0c1a995bbf4ae304
Sai19143 <88478772+Sai19143@users.noreply.github.com>
2021-08-06 13:18:36 +0530

    this is my first commit

---
+++ practice/ifelse.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int n;
+ printf("enter the number ");
+ scanf("%d",&n);
+ if (n % 2==0)
+ {
+ 	if(n % 3==0)
+ 	printf("%d divided by 2 and 3",n);
+ 
+ else
+ 
+ 	printf("%d not devided by 2 and 3",n);
+ }
+ return 0;
+}
+
+
+
+
+
+     
---
+++ practice/nestedif.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int n;
+ printf("********************************\n");
+ printf("--------------MENU--------------\n");
+ printf("********************************\n");
+ printf("1.iphone\n");
+ printf("2.oneplus\n");
+ printf("3.nokia\n");
+ scanf("%d",&n);
+ switch(n){
+ 	case 1:
+ 		printf("this is iphone");
+ 		break;
+ 	case 2:
+ 		printf("this is oneplus");
+ 		break;
+ 	case 3:
+ 		printf("this is nokia");
+ }
+ return 0;
+}
+
+
+
+
+
+     
